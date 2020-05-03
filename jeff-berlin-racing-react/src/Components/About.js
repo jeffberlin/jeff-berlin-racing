3d7d64e1bdbd47549a9b3104a8676fa06b153dd6
Jeff Berlin <jeff@jbrally.com>
2020-05-03 12:47:01 -0400

    Building About.js

--- jeff-berlin-racing-react/src/Components/About.js
+++ jeff-berlin-racing-react/src/Components/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Social from './Social';
 
 function About() {
   return (
-    <div className="section">
-      <h2 className="section-header">About</h2>
+    <div classNam="row">
+      <Social />
+      <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12 order-lg-1 justify-content-center justify-content-md-start">
+        <div className="section about">
+          <h2 className="section-header">About</h2>
+          <p>Jeff was born in January 1991 in Wilmington, Delaware. Since an early age, he was constantly playing sports and trying to get his andrenaline fix.</p>
+        </div>
+      </div>
     </div>
   )
 }
