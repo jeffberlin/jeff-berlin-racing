aab896e2da361d560c9d3089b3f8a7a5720d33de
Jeff Berlin <jeff@jbrally.com>
2020-05-12 18:51:37 -0400

    Removing folder

---
+++ build/precache-manifest.45403f205a951be5256064294b0eb665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dc7114c15bb4a92090a4574cb57618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd418898b89b070a2fd8",
+    "url": "/static/css/2.d409d1d9.chunk.css"
+  },
+  {
+    "revision": "4c10fe096a8fd5b0975c",
+    "url": "/static/css/main.e7cbb8e8.chunk.css"
+  },
+  {
+    "revision": "bd418898b89b070a2fd8",
+    "url": "/static/js/2.c85a5b69.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c85a5b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c10fe096a8fd5b0975c",
+    "url": "/static/js/main.c1e7051c.chunk.js"
+  },
+  {
+    "revision": "75c524aedb8255cd62e4",
+    "url": "/static/js/runtime-main.dc0622a5.js"
+  }
+]);
